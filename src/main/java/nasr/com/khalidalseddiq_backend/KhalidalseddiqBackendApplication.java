ca13f32990a443ce8180644230de54051c3ea74e
nasrwihtw <Nasr.al-Sayaghi@Student.HTW-Berlin.de>
2025-05-11 20:55:15 +0200

    first commit

---
+++ src/main/java/nasr/com/khalidalseddiq_backend/KhalidalseddiqBackendApplication.java
@@ -0,0 +1,13 @@
+package nasr.com.khalidalseddiq_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhalidalseddiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhalidalseddiqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nasr/com/khalidalseddiq_backend/KhalidalseddiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nasr.com.khalidalseddiq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhalidalseddiqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
